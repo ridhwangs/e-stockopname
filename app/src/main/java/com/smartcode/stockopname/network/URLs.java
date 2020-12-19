b02df223a2f2bd680d14013acba146b86f276b37
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2020-12-19 18:29:08 +0700

    URLs update

--- app/src/main/java/com/smartcode/stockopname/network/URLs.java
+++ app/src/main/java/com/smartcode/stockopname/network/URLs.java
@@ -2,7 +2,7 @@ package com.smartcode.stockopname.network;
 
 public class URLs {
 
-    private static final String ROOT_URL = "https://my.spsarana.com/api/v2/";
+    private static final String ROOT_URL = "http://192.168.1.6/e-sop/api/v2/";
 
     public static final String URL_SIMPAN = ROOT_URL + "signup";
     public static final String URL_LOGIN= ROOT_URL + "auth/login";
